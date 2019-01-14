0cd8f9bf2ce94f74abdb2ee9a783f1591c708e49
tw03 <totoramalho@gmail.com>
2019-01-14 08:01:05 -0200

    Material realizado dia 1

---
+++ todo-app/frontend/src/main/app.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './tst.css'
+
+import React from 'react'
+import Todo from '../todo/todo'
+import About from '../about/about'
+import Menu from '../template/menu'
+
+export default props => (
+    <div className='container'>
+    <Menu />
+    <Todo />
+    <About />
+    </div>
+)
