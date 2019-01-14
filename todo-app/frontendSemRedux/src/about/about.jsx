3054a6afb9c12c6d50a992e57d9b9e43fc8914c8
tw03 <totoramalho@gmail.com>
2019-01-14 16:20:44 -0200

    iniciado REACT-REDUX

---
+++ todo-app/frontendSemRedux/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='nós' />
+
+        <h2>Historia</h2>
+        <h2>BLABLALBLALB</h2>
+        <h2>Contate-nos</h2>
+        <h2>BLABLALBLALB</h2>
+        <h2>Imprensa</h2>
+        <h2>BLALBLABLA</h2>
+    </div>
+)
