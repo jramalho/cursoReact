ff4a8b31f84c4b38b2ba3b16d0a02586c100133b
tw03 <totoramalho@gmail.com>
2019-01-15 18:00:39 -0200

    Fim dia15 Aula133

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Direitos reservados pra ngm, nem pra mim.
+            <a href='http://www.google.com.br' target='_blank'> Site do Google</a> 
+        </strong>
+    </footer>
+)
