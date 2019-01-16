47112037999086d1a11376ead732ec28918b0007
tw03 <totoramalho@gmail.com>
2019-01-16 16:28:33 -0200

    finalizado frontend

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -11,8 +11,7 @@ import TabsHeader from '../common/tab/tabsHeader'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 
-import { selectTab, showTabs } from '../common/tab/tabActions'
-import { create } from './billingCycleActions'
+import { init, create, update, remove } from './billingCycleActions'
 
 import List from './billingCycleList'
 import Form from './billingCycleForm'
@@ -20,8 +19,7 @@ import Form from './billingCycleForm'
 class BillingCycle extends Component {
     
     componentWillMount() {
-        this.props.selectTab('tabList')
-        this.props.showTabs('tabList','tabCreate')
+        this.props.init()
     }
     
     render(){
@@ -39,12 +37,17 @@ class BillingCycle extends Component {
                     <TabsContent>
                         <TabContent id='tabList'><List/></TabContent>
                         <TabContent id='tabCreate'>
-                        <Form onSubmit={this.props.create}/>
+                        <Form onSubmit={this.props.create}
+                            submitLabel='Incluir' submitClass='primary'/>
                         </TabContent>
                         <TabContent id='tabUpdate'>
-                        <Form/>
+                        <Form onSubmit={this.props.update}
+                            submitLabel='Alterar' submitClass='info'/>
                         </TabContent>
-                        <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
+                        <TabContent id='tabDelete'>
+                        <Form onSubmit={this.props.remove} readOnly={true}
+                            submitLabel='Excluir' submitClass='danger'/>
+                        </TabContent>
                     </TabsContent>
                     </Tabs>
                 </Content>
@@ -55,6 +58,6 @@ class BillingCycle extends Component {
 
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-    selectTab,showTabs,
-    create}, dispatch)
+    init,
+    create, update, remove}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
--- my-money-app/frontend/src/billingCycle/billingCycleActions.js
+++ my-money-app/frontend/src/billingCycle/billingCycleActions.js
@@ -4,7 +4,7 @@ import { reset as resetForm, initialize } from 'redux-form'
 import { showTabs, selectTab} from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3000/api'
-const INITIAL_VALUES = {}
+const INITIAL_VALUES = {credits: [{}], debts: [{}]}
 
 export function getList(){
     const request = axios.get(`${BASE_URL}/billingCycles`)
@@ -15,8 +15,21 @@ export function getList(){
 }
 
 export function create(values){
-    return dispatch => { 
-        axios.post(`${BASE_URL}/billingCycles`, values)
+    return submit(values, 'post')
+}
+
+export function update(values){
+    return submit(values, 'put')
+}
+
+export function remove(values){
+    return submit(values, 'delete')
+}
+
+function submit(values, method){
+    return dispatch => {
+        const id = values._id ? values._id : '' 
+        axios[method](`${BASE_URL}/billingCycles/${id}`, values)
         .then(resp => {
             toastr.success('Sucesso', 'Operação Realizada com sucesso.')
             dispatch(init())
@@ -28,6 +41,7 @@ export function create(values){
         type: 'TEMP'
     }
     }
+    
 }
 
 export function showUpdate(billingCycle){
@@ -35,6 +49,14 @@ export function showUpdate(billingCycle){
         showTabs('tabUpdate'),
         selectTab('tabUpdate'),
         initialize('billingCycleForm', billingCycle)
+    ]
+}
+
+export function showDelete(billingCycle){
+    return [
+        showTabs('tabDelete'),
+        selectTab('tabDelete'),
+        initialize('billingCycleForm', billingCycle)   
     ]
 }
 
