414ca5c8ca628c02e954fab343b78e84140d0b20
tw03 <totoramalho@gmail.com>
2019-01-16 12:00:55 -0200

    final do meio dia AULA 145

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -4,13 +4,18 @@ import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+
 import Tabs from '../common/tab/tabs'
 import TabsContent from '../common/tab/tabsContent'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
-import { selectTab, showTabs } from '../common/tab/tabActions'
 
+import { selectTab, showTabs } from '../common/tab/tabActions'
+import { create } from './billingCycleActions'
+
+import List from './billingCycleList'
+import Form from './billingCycleForm'
 
 class BillingCycle extends Component {
     
@@ -32,9 +37,13 @@ class BillingCycle extends Component {
                         <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
                     </TabsHeader>
                     <TabsContent>
-                        <TabContent id='tabList'><h1>Lista</h1></TabContent>
-                        <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
-                        <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                        <TabContent id='tabList'><List/></TabContent>
+                        <TabContent id='tabCreate'>
+                        <Form onSubmit={this.props.create}/>
+                        </TabContent>
+                        <TabContent id='tabUpdate'>
+                        <Form/>
+                        </TabContent>
                         <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                     </TabsContent>
                     </Tabs>
@@ -45,5 +54,7 @@ class BillingCycle extends Component {
 }
 
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab,showTabs}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({
+    selectTab,showTabs,
+    create}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
---
+++ readme.md
@@ -0,0 +1,4 @@
+# cursoReact
+CursoReact
+
+Projetos do curso react da udemy "React + Redux : Fundamentos e duas APPs do Zero!"
