99644cdf6fab4586ce3463eb0a9eac4f2e2fe197
tw03 <totoramalho@gmail.com>
2019-01-18 11:17:01 -0200

    curso REACT terminado

---
+++ crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,7 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        Desenvolvido com <i className='fa fa-heart text-danger'></i> por <strong>Eu m<span className='text-danger'>3</span>smo</strong>
+    </footer>
